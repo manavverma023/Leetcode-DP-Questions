8057631cf744e1427108a29e07cc28de3a8200ed
Manav Verma <61238255+manavverma023@users.noreply.github.com>
2022-03-06 00:38:30 +0530

    Create 55. Jump Game.py

---
+++ 55. Jump Game.py
@@ -0,0 +1,18 @@
+'''
+*******************************************
+
+https://leetcode.com/problems/jump-game/
+
+******************************************
+'''
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        ans=0
+        for i in range(len(nums)):
+            if i>ans:
+                return False
+            ans=max(ans,i+nums[i])
+        return True
+            
+        
